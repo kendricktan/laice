e9d11ccc2d9044749bf6df09e46afc29eb9c228c
kendricktan <kendricktan0814@gmail.com>
2016-10-20 23:57:08 +1000

    🚀 changed CSS and using Spacy
    CSS -> Teal + Pink
    Spacy -> Named Entity Modelling

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Our story
+class Story(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+# Each story has a set of attributes
+class StoryAttribute(models.Model):
+    story = models.ForeignKey(Story)
+    attribute = models.CharField(max_length=128, null=False)
